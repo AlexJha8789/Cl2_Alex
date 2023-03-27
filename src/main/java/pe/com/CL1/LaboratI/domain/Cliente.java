dc957c5b9ce202003781a8d478a7444dd871a99f
unknown <alexjha.878@gmail.com>
2023-03-26 23:46:40 -0500

    add Entity class Cliente to project

---
+++ src/main/java/pe/com/CL1/LaboratI/domain/Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.com.CL1.LaboratI.domain;
+
+import javax.persistence.Entity;
+import lombok.Data;
+
+
+@Entity 
+@Data
+public class Cliente {
+    
+}
