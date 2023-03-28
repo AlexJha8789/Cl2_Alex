d9a7703252c4473e11951b28c7372f4804fc30fa
unknown <alexjha.878@gmail.com>
2023-03-28 15:20:18 -0500

    add Entity Class Empleado to Project

---
+++ src/main/java/pe/com/CL1/LaboratI/domain/Empleado.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.com.CL1.LaboratI.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+
+public class Empleado {
+    private long id;
+    
+}
